fbb73531482db035b4eb227e03e5c0172f98e21d
Waley Chen <wchen@redwoodstrategic.com>
2012-08-19 04:39:03 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vekii20::Application.config.secret_token = 'df8087c1ac5dae182311b575da88a3319b68db3a1f21b2c9727766e4d6b8a8a77a644343336fdbbbfeac153bfeb05f8728192d0f23b33e74de20c250db1de41a'
